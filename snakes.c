c59e22541ab2608e91e0a91d1d3383a179f92f42
William Tao <williamtao345@gmail.com>
2020-06-05 03:26:49 +0800

    fixed snakes.c; added Calculat Factorial function

--- snakes.c
+++ snakes.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include <unistd.h>
+
+// #define CALCULATE_FACTORIAL
 
 typedef long long TYPE_OF_WASTE_SPACE;
 typedef long long TYPE_OF_TOTAL_SUM;
@@ -49,13 +54,13 @@ int FindMaxNumberInGroup(int *nString, int headNumber, int tailNumber)
     return maxNumber;
 }
 
-TYPE_OF_WASTE_SPACE GetTotalSumWithWasteSpace(int *nString, int *numbersOfEachGroupsNumbers, int GroupNumber)
+TYPE_OF_WASTE_SPACE GetTotalSumWithWasteSpace(int *nString, int *numbersOfEachGroupsNumbers, int groupNumber)
 {
     TYPE_OF_WASTE_SPACE sum = 0;
     int headNumber = 0;
     int tailNumber = numbersOfEachGroupsNumbers[0] - 1;
     int maxNumberInGroup;
-    for (int i = 0; i < (GroupNumber); i++)
+    for (int i = 0; i < (groupNumber); i++)
     {
         maxNumberInGroup = (FindMaxNumberInGroup(nString, headNumber, tailNumber));
         headNumber += numbersOfEachGroupsNumbers[i];
@@ -65,31 +70,60 @@ TYPE_OF_WASTE_SPACE GetTotalSumWithWasteSpace(int *nString, int *numbersOfEachGr
     return sum;
 }
 
-void PrintNumberWays(int *numbers,int len)
+TYPE_OF_WASTE_SPACE TotalSumWithWasteSpace;
+
+#ifndef CALCULATE_FACTORIAL
+void PrintNumberWays(int *numbers, int len)
 {
-    for (int i = 0; i < len;i++)
+    for (int i = 0; i < len; i++)
     {
         printf("%d\t", numbers[i]);
-    }
-    printf("\n");
+    };
+    printf("%lld", TotalSumWithWasteSpace);
+    printf("\r\033[k");
 }
+#endif
 
-TYPE_OF_WASTE_SPACE TotalSumWithWasteSpace;
-int IfIsFirstTime = 1;
-int forFunction(int *nString, int nStringLength, int *numbersOfEachGroupsNumbers, int GroupNumber, int digit, int totalSum)
+#ifdef CALCULATE_FACTORIAL
+long long processNum = 0;
+long long CalculateFactorial(int num)
+{
+    long long total = 1;
+    for (int i = 2; i <= num; i++)
+    {
+        total *= i;
+    }
+    return total;
+}
+long long CalculateTotalProcessNum(int nStringLength, int groupNumber)
+{
+    return CalculateFactorial(nStringLength - 1) / (CalculateFactorial(groupNumber - 1) * CalculateFactorial(nStringLength - (groupNumber - 1)));
+}
+void *PrintProcess(void *arg)
+{
+    long long totalProcessNum = *(long long *)arg;
+    printf("Process is %lld\n", totalProcessNum);
+    while (1)
+    {
+        printf("Calculating: %lld%%", processNum / totalProcessNum);
+        sleep(2);
+        printf("\r\033[k");
+    }
+}
+#endif
+
+int forFunction(int *nString, int nStringLength, int *numbersOfEachGroupsNumbers, int groupNumber, int digit, int totalSum)
 {
     if (digit == 1)
     {
-        numbersOfEachGroupsNumbers[GroupNumber - digit] = nStringLength - totalSum;
-        // printf("%d\n", numbersOfEachGroupsNumbers[GroupNumber - digit]);
-        PrintNumberWays(numbersOfEachGroupsNumbers, GroupNumber);
-        TYPE_OF_WASTE_SPACE x = GetTotalSumWithWasteSpace(nString, numbersOfEachGroupsNumbers, GroupNumber);
-        if (IfIsFirstTime)
-        {
-            TotalSumWithWasteSpace = x;
-            IfIsFirstTime = 0;
-        }
-        else if (x > TotalSumWithWasteSpace)
+        numbersOfEachGroupsNumbers[groupNumber - digit] = nStringLength - totalSum;
+#ifdef CALCULATE_FACTORIAL
+        processNum++;
+#else
+        PrintNumberWays(numbersOfEachGroupsNumbers, groupNumber);
+#endif
+        TYPE_OF_WASTE_SPACE x = GetTotalSumWithWasteSpace(nString, numbersOfEachGroupsNumbers, groupNumber);
+        if (x > TotalSumWithWasteSpace)
         {
             ;
         }
@@ -103,13 +137,12 @@ int forFunction(int *nString, int nStringLength, int *numbersOfEachGroupsNumbers
         int i = 1;
         while (1)
         {
-            // printf("%d", i);
             if ((nStringLength - totalSum - i) < (digit - 1))
             {
                 break;
             }
-            numbersOfEachGroupsNumbers[GroupNumber - digit] = i;
-            forFunction(nString, nStringLength, numbersOfEachGroupsNumbers, GroupNumber, digit - 1, totalSum + i);
+            numbersOfEachGroupsNumbers[groupNumber - digit] = i;
+            forFunction(nString, nStringLength, numbersOfEachGroupsNumbers, groupNumber, digit - 1, totalSum + i);
             i++;
         };
     };
@@ -117,22 +150,51 @@ int forFunction(int *nString, int nStringLength, int *numbersOfEachGroupsNumbers
     return 1;
 }
 
-void FormLeastWasteSpace(int *nString, int nStringLength, int GroupNumber, TYPE_OF_TOTAL_SUM stringSum)
+int forFunctionFirstTime(int *nString, int nStringLength, int *numbersOfEachGroupsNumbers, int groupNumber, int digit, int totalSum)
 {
-    int *numbersOfEachGroupsNumbers = (int *)calloc(GroupNumber, sizeof(int));
-    forFunction(nString, nStringLength, numbersOfEachGroupsNumbers, GroupNumber, GroupNumber, 0);
+    if (digit == 1)
+    {
+        numbersOfEachGroupsNumbers[groupNumber - digit] = nStringLength - totalSum;
+#ifdef CALCULATE_FACTORIAL
+        processNum++;
+#else
+        PrintNumberWays(numbersOfEachGroupsNumbers, groupNumber);
+#endif
+        TYPE_OF_WASTE_SPACE x = GetTotalSumWithWasteSpace(nString, numbersOfEachGroupsNumbers, groupNumber);
+        TotalSumWithWasteSpace = x;
+    }
+    else
+    {
+        numbersOfEachGroupsNumbers[groupNumber - digit] = 1;
+        forFunction(nString, nStringLength, numbersOfEachGroupsNumbers, groupNumber, digit - 1, totalSum + 1);
+    };
+    return 1;
+}
+
+void FormLeastWasteSpace(int *nString, int nStringLength, int groupNumber, TYPE_OF_TOTAL_SUM stringSum)
+{
+    int *numbersOfEachGroupsNumbers = (int *)calloc(groupNumber, sizeof(int));
+    forFunctionFirstTime(nString, nStringLength, numbersOfEachGroupsNumbers, groupNumber, groupNumber, 0);
+    forFunction(nString, nStringLength, numbersOfEachGroupsNumbers, groupNumber, groupNumber, 0);
 }
 
 int main()
 {
     printf("--------------------\n");
-    FILE *file = fopen("input_for_snakes.txt", "r");
+    FILE *file = fopen("/Users/william/Downloads/snakes_gold_open19/3.in", "r");
     int nStringLength = GetStringLength(file);
-    int GroupNumber = GetChangingTimes(file) + 1;
+    int groupNumber = GetChangingTimes(file) + 1;
     int *nString;
     nString = (int *)calloc(nStringLength, sizeof(int));
     GetnStringFromFile(nString, file, nStringLength);
     TYPE_OF_TOTAL_SUM stringSum = GetTotalSum(nString, nStringLength);
-    FormLeastWasteSpace(nString, nStringLength, GroupNumber, stringSum);
+
+#ifdef CALCULATE_FACTORIAL
+    pthread_t calculateProcess;
+    long long totalProcessNum = CalculateTotalProcessNum(nStringLength, groupNumber);
+    pthread_create(&calculateProcess, NULL, PrintProcess, &totalProcessNum);
+#endif
+
+    FormLeastWasteSpace(nString, nStringLength, groupNumber, stringSum);
     printf("%lld - %lld = %lld\n", TotalSumWithWasteSpace, stringSum, TotalSumWithWasteSpace - stringSum);
 }

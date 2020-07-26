9296f234330203550beb03824e0d4de92d411cf6
William Tao <williamtao345@gmail.com>
2020-07-26 17:34:44 +0800

    Fix arithmetic_sequence_consisting_of_prime_numbers.c

--- arithmetic_sequence_consisting_of_prime_numbers.c
+++ arithmetic_sequence_consisting_of_prime_numbers.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-int IfPrimeNumber(int number)
+/* int IfPrimeNumber(int number)
 {
     if (number == 2)
     {
@@ -110,4 +111,129 @@ int main()
     longestarithmeticSequence = (int *)calloc(primeNumbersLen, sizeof(int));
     int longestarithmeticSequenceLen = FindLongestArithmeticSequence(longestarithmeticSequence, primeNumbers, primeNumbersLen);
     PrintIntString(longestarithmeticSequence, longestarithmeticSequenceLen);
+} */
+
+int IfPrimeNumber(int number)
+{
+    if (number == 2)
+    {
+        return 1;
+    }
+    else if (number == 1)
+    {
+        return 0;
+    }
+    else
+    {
+        for (int i = 2; i < number / 2; i++)
+        {
+            if (number % i == 0)
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
+
+typedef int TYPE_OF_NUMBER_RANGE;
+
+int FindPrimeNumbers(TYPE_OF_NUMBER_RANGE *primeNumbers, TYPE_OF_NUMBER_RANGE head, TYPE_OF_NUMBER_RANGE tail)
+{
+    int primeNumbersLen = 0;
+    for (int x = head; x <= tail; x++)
+    {
+        if (IfPrimeNumber(x))
+        {
+            primeNumbers[primeNumbersLen] = x;
+            primeNumbersLen++;
+        }
+    };
+    return primeNumbersLen;
+}
+
+int IfIsArithmeticSequence(TYPE_OF_NUMBER_RANGE *numbers, int length)
+{
+    if (length <= 1)
+    {
+        return 1;
+    }
+    else
+    {
+        TYPE_OF_NUMBER_RANGE difference = numbers[1] - numbers[0];
+        for (int i = 2; i < length; i++)
+        {
+            if ((numbers[2] - numbers[1]) != difference)
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
+
+int FindArithmeticSequenceWithSpecificFirstNumber(TYPE_OF_NUMBER_RANGE *LongestArithmeticSequence, TYPE_OF_NUMBER_RANGE *primeNumbers, int primeNumbersLen, int numberNumber, int difference)
+{
+    int x = 2;
+    int num = primeNumbers[numberNumber];
+    for (int i = numberNumber + 1; i < primeNumbersLen; i++)
+    {
+        if (primeNumbers[i] - num == difference)
+        {
+            LongestArithmeticSequence[x] = primeNumbers[i];
+            num = primeNumbers[i];
+            x++;
+        }
+    }
+    //printf("%d;\n",x);
+    return x;
+}
+
+void printASNumbers(int *ASNum)
+{
+    int x = ASNum[0];
+    for (int i = 0; i < ASNum[2]; i++)
+    {
+        printf("%d; ", x);
+        x += ASNum[1];
+    }
+    printf("\n");
+}
+
+int FindLongestArithmeticSequence(TYPE_OF_NUMBER_RANGE *longestArithmeticSequence, TYPE_OF_NUMBER_RANGE *primeNumbers, int primeNumbersLen)
+{
+    int ASLen = 0;
+    int len = 0;
+    int ASNum[3]; //In the string: N0 is fisrt number of the string; N1 is the difference between each number; N2 is the length of the string;
+    for (int numberNumber = 0; numberNumber < primeNumbersLen - 1; numberNumber++)
+    {
+        for (int i = numberNumber + 1; i < primeNumbersLen; i++)
+        {
+            len = FindArithmeticSequenceWithSpecificFirstNumber(longestArithmeticSequence, primeNumbers, primeNumbersLen, i, primeNumbers[i] - primeNumbers[numberNumber]);
+            if (len > ASLen)
+            {
+                ASNum[0] = primeNumbers[numberNumber];
+                ASNum[1] = primeNumbers[i] - primeNumbers[numberNumber];
+                ASNum[2] = len;
+                ASLen = len;
+            }
+        }
+    };
+    printASNumbers(ASNum);
+    free(ASNum);
+    return ASLen;
+};
+
+int main()
+{
+    const TYPE_OF_NUMBER_RANGE head = 100;
+    const TYPE_OF_NUMBER_RANGE tail = 10000;
+
+    TYPE_OF_NUMBER_RANGE *primeNumbers, *longestArithmeticSequence;
+    primeNumbers = (TYPE_OF_NUMBER_RANGE *)calloc((tail - head) / 2, sizeof(TYPE_OF_NUMBER_RANGE));
+    int primeNumbersLen = FindPrimeNumbers(primeNumbers, head, tail);
+    longestArithmeticSequence = (TYPE_OF_NUMBER_RANGE *)calloc(primeNumbersLen, sizeof(TYPE_OF_NUMBER_RANGE));
+    printf("%d;\n", FindLongestArithmeticSequence(longestArithmeticSequence, primeNumbers, primeNumbersLen));
+    free(primeNumbers);
+    free(longestArithmeticSequence);
 }

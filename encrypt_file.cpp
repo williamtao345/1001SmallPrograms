8ac978c3f824e849a3b059b51695bf7ac5bb1e04
William Tao <williamtao345@gmail.com>
2020-07-31 13:01:33 +0800

    Fixed encrypt_file.cpp

--- encrypt_file.cpp
+++ encrypt_file.cpp
@@ -1,11 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef char TYPE_OF_ENCRYPT;
+typedef int TypeOfEncrypt;
+#define NEW_ENCRYPT_FILE
+#define SOURCE_FILE_NAME "draft.cpp"
+#define ENCRYPT_FILE_NAME "draft1.cpp"
+#define DECRYPT_FILE_NAME "draft2.cpp"
+#define CLEAN_SCREEN printf("\x1b[H\x1b[2J")
+// #define WINDOWS
+#define TEST
 
-// #define TEST
 #ifdef TEST
-#define PASSWORD 2
+#define PASSWORD 123
 #else
 #define PASSWORD GetPassword()
 #endif
@@ -13,7 +19,9 @@ typedef char TYPE_OF_ENCRYPT;
 char GetKey()
 {
     char input;
+#ifndef WINDOWS
     system("stty -icanon");
+#endif
     input = getchar();
     if (input == '\033')
     {
@@ -23,25 +31,40 @@ char GetKey()
             input = getchar();
         }
     }
+#ifndef WINDOWS
     system("stty icanon");
+#endif
     printf("\r\033[k");
     return input;
 }
 
 void ShowPage1()
 {
-    printf("\x1b[H\x1b[2J");
+    CLEAN_SCREEN;
     printf("Encrypt File <-\n");
     printf("Decrypt File\n");
 }
 
 void ShowPage2()
 {
-    printf("\x1b[H\x1b[2J");
+    CLEAN_SCREEN;
     printf("Encrypt File\n");
     printf("Decrypt File <-\n");
 }
 
+#ifdef WINDOWS
+int VisualPage()
+{
+    printf("Input 1 to encrypt file;\n");
+    printf("Input 2 to decrypt file;\n");
+    printf("Input -1 to quit;\n")
+        printf("Your choice: ");
+    fflush(stdout);
+    char choice;
+    scanf("%c", &choice);
+    return choice;
+}
+#else
 int VisualPage()
 {
     ShowPage1();
@@ -60,7 +83,7 @@ int VisualPage()
             choice = 0;
             break;
         case '\n':
-            printf("\x1b[H\x1b[2J");
+            CLEAN_SCREEN;
             return choice;
         default:
             switch (choice)
@@ -76,15 +99,15 @@ int VisualPage()
         }
         input = GetKey();
     }
-    printf("\x1b[H\x1b[2J");
+    CLEAN_SCREEN;
     return EOF;
 }
-
+#endif
 char *GetSourceFileName()
 {
     char *sourceFileName;
     sourceFileName = (char *)calloc(20, sizeof(char));
-    printf("Input source file name :");
+    printf("Input source file name  : ");
     fflush(stdout);
     scanf("%s", sourceFileName);
     return sourceFileName;
@@ -94,24 +117,42 @@ char *GetEncryptFileName()
 {
     char *encryptFileName;
     encryptFileName = (char *)calloc(20, sizeof(char));
-    printf("Input encrypt file name :");
+    printf("Input encrypt file name : ");
     fflush(stdout);
     scanf("%s", encryptFileName);
     return encryptFileName;
 }
 
-TYPE_OF_ENCRYPT GetPassword()
+TypeOfEncrypt GetPassword()
 {
-    TYPE_OF_ENCRYPT password;
+    TypeOfEncrypt password;
     printf("Input password: ");
     fflush(stdout);
     fflush(stdin);
-    scanf("%c", &password);
+    int x;
+    scanf("%d", &x);
+    password = (TypeOfEncrypt)x;
     return password;
+}
+
+int SizeOfFile(FILE *file)
+{
+    fseek(file, 0L, SEEK_END);
+    int count = ftell(file);
+    rewind(file);
+    return count;
 }
 
 void EncryptFile()
 {
+
+#ifdef TEST
+    FILE *sourceFile;
+    sourceFile = fopen(SOURCE_FILE_NAME, "rb");
+
+    FILE *encryptFile;
+    encryptFile = fopen(ENCRYPT_FILE_NAME, "wb");
+#else
     FILE *sourceFile;
     char *sourceFileName;
     do
@@ -125,16 +166,30 @@ void EncryptFile()
     {
         encryptFileName = GetEncryptFileName();
     } while ((encryptFile = fopen(encryptFileName, "wb")) == NULL);
+#endif
 
-    TYPE_OF_ENCRYPT password = PASSWORD;
-    TYPE_OF_ENCRYPT info;
-    fread(&info, sizeof(TYPE_OF_ENCRYPT), 1, sourceFile);
+    TypeOfEncrypt password = PASSWORD;
+    TypeOfEncrypt info;
+    int i;
     while (!feof(sourceFile))
+#ifdef NEW_ENCRYPT_FILE
     {
+        i = fread(&info, sizeof(char), sizeof(TypeOfEncrypt), sourceFile);
         info = password ^ info;
-        fwrite(&info, sizeof(TYPE_OF_ENCRYPT), 1, encryptFile);
-        fread(&info, sizeof(TYPE_OF_ENCRYPT), 1, sourceFile);
+        fwrite(&info, sizeof(char), sizeof(TypeOfEncrypt), encryptFile);
     }
+    info = 8 - i;
+    info = password ^ info;
+    fwrite(&info, sizeof(int), 1, encryptFile);
+
+#else
+        while (!feof(sourceFile))
+        {
+            fread(&info, sizeof(char), sizeof(TypeOfEncrypt), sourceFile);
+            info = password ^ info;
+            fwrite(&info, sizeof(char), sizeof(TypeOfEncrypt), encryptFile);
+        }
+#endif
 
     fclose(encryptFile);
     fclose(sourceFile);
@@ -142,29 +197,62 @@ void EncryptFile()
 
 void DecryptFile()
 {
+
+#ifdef TEST
+    FILE *encryptFile;
+    encryptFile = fopen(ENCRYPT_FILE_NAME, "rb");
+
+    FILE *sourceFile;
+    sourceFile = fopen(DECRYPT_FILE_NAME, "wb");
+#else
     FILE *encryptFile;
     char *encryptFileName;
     do
     {
         encryptFileName = GetEncryptFileName();
     } while ((encryptFile = fopen(encryptFileName, "rb")) == NULL);
-
     FILE *sourceFile;
     char *sourceFileName;
     do
     {
         sourceFileName = GetSourceFileName();
     } while ((sourceFile = fopen(sourceFileName, "wb")) == NULL);
+#endif
 
-    TYPE_OF_ENCRYPT password = PASSWORD;
-    TYPE_OF_ENCRYPT info;
-    fread(&info, sizeof(TYPE_OF_ENCRYPT), 1, encryptFile);
+    TypeOfEncrypt password = PASSWORD;
+    TypeOfEncrypt info;
+
+#ifdef NEW_ENCRYPT_FILE
+    int fileSize = SizeOfFile(encryptFile);
+
+    FILE *tmpFp = fopen("tmpFile.txt", "wb");
+    for (int i = 0; i < fileSize / sizeof(TypeOfEncrypt); i++)
+    {
+        fread(&info, sizeof(TypeOfEncrypt), 1, encryptFile);
+        info = info ^ password;
+        fwrite(&info, sizeof(TypeOfEncrypt), 1, tmpFp);
+    }
+
+    fileSize -= info;
+
+    freopen("tmpFile.txt", "rb", tmpFp);
+    for (int i = 0; i < fileSize; i++)
+    {
+        int x = ftell(tmpFp);
+        int y = fread(&info, sizeof(char), 1, tmpFp);
+        fwrite(&info, sizeof(char), 1, sourceFile);
+    }
+    fclose(tmpFp);
+    remove("tmpFile.txt");
+
+#else
     while (!feof(encryptFile))
     {
+        fread(&info, sizeof(TypeOfEncrypt), 1, encryptFile);
         info = info ^ password;
-        fwrite(&info, sizeof(TYPE_OF_ENCRYPT), 1, sourceFile);
-        fread(&info, sizeof(TYPE_OF_ENCRYPT), 1, encryptFile);
+        fwrite(&info, sizeof(TypeOfEncrypt), 1, sourceFile);
     }
+#endif
 
     fclose(encryptFile);
     fclose(sourceFile);
@@ -172,10 +260,22 @@ void DecryptFile()
 
 int main()
 {
-    while (1)
+#ifdef TEST
+    EncryptFile();
+    DecryptFile();
+#else
+    while (true)
     {
-        printf("**********type Y to exit**********");
+        printf("Reminder: Type Y to exit;\n");
+        printf("Type any key to continue: ");
+#ifndef WINDOWS
+        system("stty -icanon");
+#endif
         getchar();
+#ifndef WINDOWS
+        system("stty icanon");
+#endif
+        fflush(stdin);
         int selection = VisualPage();
         if (selection == EOF)
         {
@@ -193,4 +293,5 @@ int main()
             break;
         }
     }
+#endif
 }
